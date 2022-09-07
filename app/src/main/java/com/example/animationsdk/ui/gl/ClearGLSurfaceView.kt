3d87d8021bceb37ebdf29beb71a3124195d9214c
Ivantsov Mykola <ivantsov.nikolai.01@gmail.com>
2022-09-07 22:55:30 +0300

    added the get info about GL versions

--- app/src/main/java/com/example/animationsdk/ui/gl/ClearGLSurfaceView.kt
+++ app/src/main/java/com/example/animationsdk/ui/gl/ClearGLSurfaceView.kt
@@ -1,9 +1,13 @@
 package com.example.animationsdk.ui.gl
 
 import android.content.Context
+import android.opengl.GLES20.*
+import android.opengl.GLES30
 import android.opengl.GLSurfaceView
 import android.util.AttributeSet
+import android.util.Log
 import com.example.animationsdk.ui.gl.startAndroid.OpenGLRenderer
+import java.nio.IntBuffer
 
 
 class ClearGLSurfaceView(context: Context, attrs: AttributeSet? = null) :
@@ -12,9 +16,18 @@ class ClearGLSurfaceView(context: Context, attrs: AttributeSet? = null) :
     val mRenderer = OpenGLRenderer(context)
 
     init {
-        setEGLContextClientVersion(2)
+        setEGLContextClientVersion(3)
         setRenderer(mRenderer)
-//       setRenderMode(GLSurfaceView.RENDERMODE_WHEN_DIRTY)
+        val render = glGetString(GLES30.GL_RENDERER)
+        val vendor = glGetString(GLES30.GL_VENDOR)
+        val version = glGetString(GLES30.GL_VERSION)
+        val glslVersion = glGetString(GLES30.GL_SHADING_LANGUAGE_VERSION)
+        val major: IntBuffer = IntBuffer.allocate(100)
+        val minor: IntBuffer = IntBuffer.allocate(100)
+        glGetIntegerv(GLES30.GL_MAJOR_VERSION, major)
+        glGetIntegerv(GLES30.GL_MINOR_VERSION, minor)
+
+        Log.d("TAG_7", "render: $render, vendor: $vendor, version: $version, , major: ${major}, minor: $minor, glslVersion: $glslVersion")
 
     }
 }
